f9093158f9ed6e5e206b4abe20b0d25ba24255df
Leafoliage <leafoliages@gmail.com>
2023-01-11 13:45:51 +0800

    [mod] text-scrape & commands.csv

--- text-scrape.py
+++ text-scrape.py
@@ -1,13 +1,46 @@
 import requests
-import os
 from dotenv import dotenv_values
+import json
+from bs4 import BeautifulSoup
+import pandas as pd
 
 config = dotenv_values(".env")
-query = "go"
+commands_df = pd.read_csv('commands.csv')
 
-url = f"https://www.googleapis.com/customsearch/v1?cx={config['SE_ID']}&key={config['API_KEY']}&q={query}"
-r = requests.get(url)
-print('request finished')
-json = r.json()
-for res in json.item:
-    print(res.html)
+def extract_links(response):
+    j = response.json()
+    text = json.dumps(j)
+    return text.replace("{\"items\": [{\"link\": \"", "").replace("\"}]}", "").split("\"}, {\"link\": \"")
+
+def extract_text(link):
+    bs = BeautifulSoup(requests.get(link).text, "html.parser")
+    doc = ""
+    for h1 in bs.find_all('h1'):
+        doc += h1.text
+    for h2 in bs.find_all('h2'):
+        doc += h2.text
+    for h3 in bs.find_all('h3'):
+        doc += h3.text
+    for p in bs.find_all('p'):
+        doc += p.text
+    return doc
+
+def process(command):
+    q = "linux command" + command
+    fields = "items/link"
+    url = f"https://www.googleapis.com/customsearch/v1?cx={config['SE_ID']}&key={config['API_KEY']}&q={q}&fields={fields}"
+    r = requests.get(url)
+    links = extract_links(r)
+    docs = ""
+    for link in links:
+        docs += extract_text(link)
+    return docs
+
+data = []
+for cmd in commands_df['command']:
+    data.append([cmd, process(cmd)])
+    print(f" {cmd} finished \x1b[1A")
+text_df = pd.DataFrame(data=data, columns=['command', 'document'])
+text_df.to_csv('text.csv', index=False)
+print('\x1b[2K')
+print('job done')
