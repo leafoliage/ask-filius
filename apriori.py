5a61de2f33ae11bddfd8be9f1b87ca068c50e6d1
Leafoliage <leafoliages@gmail.com>
2023-01-15 21:01:28 +0800

    [mod] apriori.py

--- apriori.py
+++ apriori.py
@@ -4,7 +4,7 @@ import pandas as pd
 association_df = pd.read_csv('association.csv')
 association_df = association_df.astype(bool)
 
-frequent_sets = apriori(association_df)
+frequent_sets = apriori(association_df, use_colnames=True)
 rules = association_rules(frequent_sets, metric="lift")
 
 rules.to_csv('rules.csv')
