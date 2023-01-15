08231c2074e39dbc438b5f58c4a921a8e4f3cea5
Leafoliage <leafoliages@gmail.com>
2023-01-15 21:01:51 +0800

    [mod] main.py

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ commands_df = pd.read_csv('commands.csv')
 tfidf_df = pd.read_csv('tfidf.csv')
 rules_df = pd.read_csv('rules.csv')
 
+def str_to_set(str):
+    return frozenset(str.replace("frozenset({\'","").replace("\'})","").split("\', \'"))
+
 def subset(s):
     subs = [[]]
     for el in s:
         subs += [sub+[el] for sub in subs]
+    subs = [set(sub) for sub in subs]
     return subs
 
 def search(query):
@@ -26,15 +30,21 @@ def search(query):
     best_set = set()
     best_lift = 0
     for s in subset(first_draft):
-        top = rules_df[rules_df['antecedents']==s].sort_values(by='lift', ascending=False)[0]
+        filtered = rules_df[rules_df['antecedents']==s].sort_values(by='lift', ascending=False)
+        if len(filtered.index) == 0:
+            continue
+        top = filtered.iloc[0]
         if top['lift']>best_lift: 
-            best_set = top['consequence']
+            best_set = top['consequents']
             best_lift = top['lift']
 
     recommendation = list(first_draft)
-    recommendation.extend(list(best_set))
+    recommendation.extend(best_set.difference(first_draft))
 
     return recommendation
+
+rules_df['antecedents'] = rules_df['antecedents'].apply(lambda x: str_to_set(x))
+rules_df['consequents'] = rules_df['consequents'].apply(lambda x: str_to_set(x))
 
 print("What command are you looking for?")
 query = input("Try describe what the command does: ")
